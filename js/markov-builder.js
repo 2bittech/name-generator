665d162db8dc45d7cd902dc8754423fe81b99c8d
2bitTech <akshat@2bitTech.com>
2021-07-12 16:47:12 -0700

    Initial commit

---
+++ js/markov-builder.js
@@ -0,0 +1,22 @@
+importScripts("foswig.min.js");
+
+var chain = null;
+
+self.addEventListener('message', function(e) {
+	if (e.data.messageType === "load") {
+		//get the list of dictionary words
+		var req = new XMLHttpRequest();  
+		req.open('GET', "dictionary.json", false); 
+		req.send( null ); 
+		var data = JSON.parse(req.responseText);
+			
+		chain = new foswig.MarkovChain(3);
+
+		//load the words into the markov chain
+		chain.addWordsToChain(data.words);
+		postMessage({messageType:"loaded"});
+	}
+	else if (e.data.messageType === "generate" && chain) {
+		postMessage({messageType:"generate",word:chain.generateWord(e.data.minLength,e.data.maxLength,false)});
+	}
+},false);
